8a292744fa07b3f3539c346b2cb38ce04d234643
Sarah Mogin <mogin@work.co>
2019-03-09 22:40:35 -0500

    linting, basic math

---
+++ src/utils/math.js
@@ -0,0 +1,44 @@
+const randomInt = () => Math.ceil(Math.random() * 8) + 1;
+
+const shuffle = (a) => {
+  const copy = [...a];
+  for (let i = copy.length - 1; i > 0; i -= 1) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [copy[i], copy[j]] = [copy[j], copy[i]];
+  }
+  return copy;
+};
+
+const pickRandom = items => items[Math.floor(Math.random() * items.length)];
+
+const add = (x, y) => x + y;
+const subtract = (x, y) => x - y;
+const multiply = (x, y) => x * y;
+const divide = (x, y) => x / y;
+
+const OPS = [add, subtract, multiply, divide];
+
+const findAnAnswer = (ints) => {
+  let answer = ints[0];
+
+  ints.forEach((int, i) => {
+    if (i !== 0) {
+      answer = pickRandom(OPS)(answer, int);
+    }
+  });
+
+  return answer;
+};
+
+const math = () => {
+  const ints = [1, 2, 3, 4].map(randomInt);
+
+  let answer = findAnAnswer(ints);
+  while (answer % 1 !== 0 || answer < 1 || answer > 40) {
+    answer = findAnAnswer(ints);
+  }
+
+  return [...shuffle(ints), answer];
+};
+
+export default math;
---
+++ src/utils/math.test.js
@@ -0,0 +1,7 @@
+import math from './math';
+
+describe('math', () => {
+  it('does a thing', () => {
+    expect(math()).toEqual(expect.any(Array));
+  });
+});
