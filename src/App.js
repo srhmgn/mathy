0e80bb9ffca1317eb209286fed5d52555dcb2223
Sarah Mogin <mogin@work.co>
2019-03-10 15:25:31 -0400

    game works

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 import math, { OPS } from './utils/math';
 
-const [originalInts, answer] = math();
-
-const onDragOver = (e) => {
-  // console.log(e.currentTarget.dataset);
-  // return false;
-  e.preventDefault();
-};
 
 const App = () => {
-  const defaultItems = [
-    ...originalInts.map(value => ({ value, type: 'int' })),
-    { value: answer, type: 'answer' },
-  ];
-  const [items, setItems] = useState(defaultItems);
+  const [items, setItems] = useState(math());
   const [drag, setDrag] = useState({});
+  const [canDrag, setCanDrag] = useState(true);
+
+  const onDragOver = (e) => {
+    e.preventDefault();
+  };
+
+  let value;
+  const [
+    int1,
+    op1,
+    int2,
+    op2,
+    int3,
+    op3,
+    int4,
+    _equals,
+    answer,
+  ] = items.map(i => i.value);
+
+  if (OPS[op1] && OPS[op2] && OPS[op3]) {
+    value = OPS[op1](int1, int2);
+    value = OPS[op2](value, int3);
+    value = OPS[op3](value, int4);
+  }
+
+  const didWin = value === answer;
+
+  useEffect(() => {
+    if (didWin && canDrag) {
+      setCanDrag(false);
+      setTimeout(
+        () => {
+          setCanDrag(true);
+          setItems(math());
+        },
+        1000,
+      );
+    }
+  });
 
   const swapInts = (e, i, j) => {
     e.preventDefault();
@@ -29,19 +57,10 @@ const App = () => {
     setItems(copy);
   };
 
-  // todo better name for this func
-  // todo dont allow two ops in a row
-  const onListDrop = (e) => {
-    e.preventDefault();
-
-    if (drag.op) {
-      const foundEl = Array.from(document.querySelectorAll('.box--int, .box--answer')).find(el => (
-        el.getBoundingClientRect().right > e.clientX
-      ));
-      const copy = [...items];
-      copy.splice(foundEl.dataset.i, 0, { value: drag.op, type: 'op' });
-      setItems(copy);
-    }
+  const dropOp = (e, i) => {
+    const copy = [...items];
+    copy[i].value = drag.op;
+    setItems(copy);
   };
 
   const renderItem = (item, i) => {
@@ -49,14 +68,13 @@ const App = () => {
       return (
         <div
           className="box box--int"
-          data-i={i}
           onDragOver={onDragOver}
           onDrop={e => swapInts(e, drag.intIdx, i)}
           key={i}
         >
           <div
             className="box-content"
-            draggable
+            draggable={canDrag}
             onDragStart={e => setDrag({ el: e.target, intIdx: i })}
           >
             {item.value}
@@ -65,14 +83,28 @@ const App = () => {
       );
     } if (item.type === 'op') {
       return (
-        <div className="box box--op" data-i={i} key={i}>
+        <div
+          onDragOver={onDragOver}
+          onDrop={e => dropOp(e, i)}
+          className="box box--op"
+          key={i}
+        >
           <div className="box-content">{item.value}</div>
+        </div>
+      );
+    } if (item.type === 'equals') {
+      return (
+        <div
+          className="box box--op box--equals"
+          key={i}
+        >
+          <div className="box-content">=</div>
         </div>
       );
     }
 
     return (
-      <div className="box box--answer" data-i={i} key={i}>
+      <div className="box box--answer" key={i}>
         <div className="box-content">{item.value}</div>
       </div>
     );
@@ -82,25 +114,21 @@ const App = () => {
     <div className="wrapper">
       <div className="list list--op">
         {Object.keys(OPS).map(op => (
-          <div className="box box--op top--op-top" key={op}>
+          <div className="box box--op-top" key={op}>
             <div
               className="box-content"
-              draggable
+              draggable={canDrag}
               onDragStart={e => setDrag({ el: e.target, op })}
             >
               {op}
-
             </div>
           </div>
         ))}
       </div>
-      <div
-        className="list"
-        onDragOver={onDragOver}
-        onDrop={onListDrop}
-      >
+      <div className="list list--main">
         {items.map(renderItem)}
       </div>
+      <div>{didWin && 'You won!'}</div>
     </div>
   );
 };
--- src/utils/math.js
+++ src/utils/math.js
@@ -16,6 +16,8 @@ const subtract = (x, y) => x - y;
 const multiply = (x, y) => x * y;
 const divide = (x, y) => x / y;
 
+export const NO_OP = '';
+
 export const OPS = {
   '+': add,
   '-': subtract,
@@ -45,7 +47,45 @@ const math = () => {
     answer = findAnAnswer(ints);
   }
 
-  return [shuffle(ints), answer];
+  const shuffled = shuffle(ints);
+
+  return [
+    {
+      value: shuffled[0],
+      type: 'int',
+    },
+    {
+      value: NO_OP,
+      type: 'op',
+    },
+    {
+      value: shuffled[1],
+      type: 'int',
+    },
+    {
+      value: NO_OP,
+      type: 'op',
+    },
+    {
+      value: shuffled[2],
+      type: 'int',
+    },
+    {
+      value: NO_OP,
+      type: 'op',
+    },
+    {
+      value: shuffled[3],
+      type: 'int',
+    },
+    {
+      type: 'equals',
+    },
+    {
+      value: answer,
+      type: 'answer',
+    },
+  ];
 };
 
 export default math;
