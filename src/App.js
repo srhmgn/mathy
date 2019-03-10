3e58e8cdee08c0d3b395629f37af678e04e846a1
Sarah Mogin <mogin@work.co>
2019-03-09 23:43:04 -0500

    grid styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 
-import math from './utils/math';
+import math, { OPS } from './utils/math';
 
 const [originalInts, answer] = math();
 
@@ -19,24 +19,33 @@ const App = () => {
 
   return (
     <div className="wrapper">
-      {ints.map((int, i) => (
-        <div
-          className="box box--int"
-          onDragOver={onDragOver}
-          onDrop={() => swapInts(dragIdx, i)}
-          key={i}
-        >
-          <div
-            className="box-inner"
-            draggable
-            onDragStart={() => setDragIdx(i)}
-          >
-            {int}
+      <div className="list list--op">
+        {Object.keys(OPS).map(op => (
+          <div className="box box--op" key={op}>
+            <div className="box-content">{op}</div>
           </div>
+        ))}
+      </div>
+      <div className="list">
+        {ints.map((int, i) => (
+          <div
+            className="box box--int"
+            onDragOver={onDragOver}
+            onDrop={() => swapInts(dragIdx, i)}
+            key={i}
+          >
+            <div
+              className="box-content"
+              draggable
+              onDragStart={() => setDragIdx(i)}
+            >
+              {int}
+            </div>
+          </div>
+        ))}
+        <div className="box box--answer">
+          <div className="box-content">{answer}</div>
         </div>
-      ))}
-      <div className="box box--answer">
-        <div className="box-inner">{answer}</div>
       </div>
     </div>
   );
